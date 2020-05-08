095bc39c9ec893a9925edd6df6d548aff94cd905
PauloCarvalho96 <37113@ufp.edu.pt>
2020-05-08 22:08:34 +0100

    teste

---
+++ src/edu/ufp/inf/sd/rmi/projeto/client/LoadGui.java
@@ -0,0 +1,25 @@
+package edu.ufp.inf.sd.rmi.projeto.client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoadGui extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Scene scene = new Scene(root, 601, 402);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+}
