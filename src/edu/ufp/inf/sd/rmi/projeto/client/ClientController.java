8c2a67f2b1e561fbf9e65f63b1551df352bd9a15
Toni <36644@ufp.edu.pt>
2020-05-12 01:42:24 +0100

    login register fxml ids and handlers in controller

--- src/edu/ufp/inf/sd/rmi/projeto/client/ClientController.java
+++ src/edu/ufp/inf/sd/rmi/projeto/client/ClientController.java
@@ -8,34 +8,43 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ClientController implements Initializable {
-
+    /** Create task **/
     public Tab createTaskTab;
     public TextField nameTaskTF;
     public Button createTaskBut;
-    public ComboBox<String> typeHashCB;
+    public ComboBox<String> hashTypeCB;
     public TextField hashPassTaskTF;
     public Tab listTasksTab;
+    /** Join task **/
     public ComboBox<String> nameTasksCB;
     public Button jointTaskBut;
+    /** List tasks **/
     public TableView<String> professionalsDPTable;
     public TableColumn<String, String> nameCol;
     public TableColumn<String, String> passHashCol;
     public TableColumn<String, String> hashCol;
     public TableColumn<String, String> threadsCol;
     public TableColumn<String, String> availableCol;
+    /** Login **/
+    public PasswordField passwordLoginTF;
+    public PasswordField passwordRegisterTF;
+    public TextField usernameLoginTF;
+    /** Register **/
+    public Button registerBut;
+    public TextField usernameRegisterTF;
+    public PasswordField confirmPasswordRegisterTF;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        typeHashCB.getItems().clear();
-        typeHashCB.setPromptText("SHA-512");
-        typeHashCB.getItems().add("SHA-512");
-        typeHashCB.getItems().add("PBKDF2");
-        typeHashCB.getItems().add("BCrypt");
-        typeHashCB.getItems().add("SCrypt");
+        hashTypeCB.getItems().clear();
+        hashTypeCB.setPromptText("SHA-512");
+        hashTypeCB.getItems().add("SHA-512");
+        hashTypeCB.getItems().add("PBKDF2");
+        hashTypeCB.getItems().add("BCrypt");
+        hashTypeCB.getItems().add("SCrypt");
     }
 
     public void handleReadFile(ActionEvent actionEvent) {
-        System.out.println("olaaaaaaaaaaa");
     }
 
     public void handleReadBinFile(ActionEvent actionEvent) {
@@ -62,6 +71,13 @@ public class ClientController implements Initializable {
     public void handlerJoinTask(ActionEvent actionEvent) {
     }
 
-    public void handlerTypeHash(ActionEvent actionEvent) {
+    public void handlerHashType(ActionEvent actionEvent) {
+    }
+
+    public void handlerRegister(ActionEvent actionEvent) {
+        
+    }
+
+    public void handlerLogin(ActionEvent actionEvent) {
     }
 }
