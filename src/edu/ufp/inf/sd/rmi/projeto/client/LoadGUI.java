e0f45eae93b6bd2965a3a9367ef0ec61fa9ace3b
Toni <36644@ufp.edu.pt>
2020-05-15 23:20:20 +0100

    az

---
+++ src/edu/ufp/inf/sd/rmi/projeto/client/LoadGUI.java
@@ -0,0 +1,25 @@
+package edu.ufp.inf.sd.rmi.projeto.client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class LoadGUI extends Application {
+
+    public static void main(String[] args) { launch(args); }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("layouts/authentication.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Authentication");
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream( "logo/logo.png" )));
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+}
